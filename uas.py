5c35fe9929d9fa79846d3ed6b1c9629fd49696a4
fajar67878 <fajarramdani0405@gmail.com>
2025-06-02 10:17:50 +0700

    Delete uas.py

--- uas.py
+++
@@ -1,9 +0,0 @@
-# Program menghitung nilai rata-rata dari list bilangan
-bilangan = [10, 20, 30, 40, 50]  # Contoh list bilangan
-
-jumlah = sum(bilangan)
-banyak = len(bilangan)
-rata_rata = jumlah / banyak
-
-print("List bilangan:", bilangan)
-print("Nilai rata-rata:", rata_rata)
